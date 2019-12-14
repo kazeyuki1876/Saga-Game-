90b04d282db611a05e4097002df1787e435212aa
kazeyuki1876 <fengxue1876@gmail.com>
2019-12-14 16:29:04 +0900

    HPゲージ

--- Saga/Assets/Scripts/Gunner/GunnerMove/New/GunnaerHealth.cs
+++ Saga/Assets/Scripts/Gunner/GunnerMove/New/GunnaerHealth.cs
@@ -7,14 +7,25 @@ using UnityEngine.UI;
 
 public class GunnaerHealth : MonoBehaviour
 {
+    public float myHpMax = 100;
     public float MyHp = 100;
+    [SerializeField]
+    private float fillAmountSpeed = 0.5f;
     //魔石数				
-  public int MyMagicStone = 0;
+    public int MyMagicStone = 0;
 
     public Text PlayerHp, PlayerMagicStone;
+    [SerializeField]
+    private Image
+        GannerHpGaugeRed,
+        GannerHpGaugeGreen;
+
+
+
     // Start is called before the first frame update				
     void Start()
     {
+        MyHp = myHpMax;
         //HP=MAXHP				
     }
 
@@ -23,6 +34,7 @@ public class GunnaerHealth : MonoBehaviour
     {
         PlayerHp.text = "生命" + MyHp;
         PlayerMagicStone.text = "魔石" + MyMagicStone;
+        GannerHpGaugeMove();
     }
     void OnTriggerEnter(Collider MagicStone)
     {
@@ -35,6 +47,21 @@ public class GunnaerHealth : MonoBehaviour
             Destroy(MagicStone.gameObject);  // MyMagicStoneを崩壊				
             MyMagicStone++;
         }
+    }
+    void GannerHpGaugeMove(){
+      
+        //-
+        GannerHpGaugeGreen.fillAmount = MyHp / myHpMax;
+        if (GannerHpGaugeRed.fillAmount > GannerHpGaugeGreen.fillAmount)
+        {
+            GannerHpGaugeRed.fillAmount = GannerHpGaugeRed.fillAmount - (GannerHpGaugeRed.fillAmount - GannerHpGaugeGreen.fillAmount) * fillAmountSpeed * Time.deltaTime;
+        }
+        else
+        {
+            GannerHpGaugeRed.fillAmount = GannerHpGaugeGreen.fillAmount;
+        }
+
+
     }
 }
 
