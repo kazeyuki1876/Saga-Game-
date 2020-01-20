f237cc6594b6d98c861ea6530c55c7dc15d884e4
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2020-01-20 23:40:11 +0900

    MagicStoneGameObject

--- Saga/Assets/Monsu/MagicStoneScript.cs
+++ Saga/Assets/Monsu/MagicStoneScript.cs
@@ -6,19 +6,15 @@ public class MagicStoneScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject fatherGameObject;
-
     /// <summary>
     ///魔石が回るスクリプト
     [SerializeField]
     private float MySpeed=20.0f;
-    // Start is called before the first frame update
-    // Update is called once per frame
     private void Update()
     {
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + MySpeed * Time.deltaTime, 0);
     }
     private void SuctionGameObjectOnTriggerStay(Collider col) {
-
     }
     public void DestroyFather() {
     Destroy(fatherGameObject);  
